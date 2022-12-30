9733655c3978b0c8ab39955fa758e285da2a4e9a
rayhan-rafin <rayhan.rafin2@gmail.com>
2022-12-30 20:45:18 +0600

    Update Forming a Magic Square.cpp

--- Algorithm/Implementation/Forming a Magic Square.cpp
+++ Algorithm/Implementation/Forming a Magic Square.cpp
@@ -3,22 +3,33 @@ using namespace std;
 
 int main ()
 {
-    int s[3][3];
-    int a;
-    vector<int>dup;
-    vector<int>non_exist;
+    int a,sum=0;
     int num_arr[9];
     //1-9 number occurance prep.
-    for (int i=0;i<9;i++){
-            num_arr[i] = 0;
+    for (int i=0; i<9; i++)
+    {
+        num_arr[i] = 0;
     }
-    for (int i=0;i<9;i++){
+    for (int i=0; i<9; i++)
+    {
         cin>>a;
         num_arr[a-1]++;
     }
-    for (int i=0;i<9;i++){
-        cout<<num_arr[i];
+//    for (int i=0;i<9;i++){
+//        cout<<num_arr[i];
+//    }
+//check for duplicate
+    for (int i=0; i<9; i++)
+    {
+        for (int j=0; num_arr[i]>1; j++)
+        {
+            if (num_arr[j]==0)
+            {
+                num_arr[i]--;
+                num_arr[j]=1;
+                sum = sum + abs(i-j);
+            }
+        }
     }
-
     return 0;
 }
